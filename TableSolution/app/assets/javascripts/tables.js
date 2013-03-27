e24a04b68021846e5f6226839307751936dd5c39
Avinash kaur <avinash.kaur@vinsol.com>
2013-03-28 00:42:07 +0530

    27/03

--- TableSolution/app/assets/javascripts/tables.js
+++ TableSolution/app/assets/javascripts/tables.js
@@ -1,37 +1,45 @@
 $(document).ready( function() {
-  $("#unassigned tr").each(function(){
+  $("#unassigned tr td").each(function(){
     $(this).draggable({
-      stack: '#unassigned tr',
+      helper: "clone",
+      stack: '#unassigned tr td',
       cursor: 'move',
       revert: true
-      // stop: function(event, ui) {
-
-      // }
     });
   });
 
-  $("#floor_image_div #slot_container").delegate("div", "click", function(){
-    $(this).draggable();
-  });
+  // $("#floor_image_div #slot_container").delegate("div", "drag", function(){
+  //   $(this).draggable();
+  // });
 
-  $( "#floor_image_div" ).droppable({
-    accept: '#unassigned tr',
-    drop: handleTableDrop
-  });
+  $length = $("#floor_image_div .slot").length;
+  for ( i = 1; i <= $length; i++) {
+    $("#floor_image_div #slot" + i).droppable({
+      accept: '#unassigned tr td',
+      drop: handleTableDrop,
+      hoverClass: "hovered"
+    });
+  }
+  
 
-  function handleTableDrop( event, ui ) {
-      if (!($(ui.draggable).hasClass("table_pic"))) {
-        table_value = $(ui.draggable).find("td:first-child").text();
-        $newDiv = $("<div></div>").addClass("table_pic")
-        $newDiv.append($("<label></label>").text(table_value));
-            
-        var $newPosX = ui.offset.left - $(this).offset().left;
-        var $newPosY = ui.offset.top - $(this).offset().top;
-        $newDiv.css("left", $newPosX);
-        $newDiv.css("top", $newPosY);
-        $newDiv.appendTo($(this));
-      }
-      console.log($(ui.draggable).position())
+  function handleTableDrop(event, ui) {
+    if (!($(ui.draggable).hasClass("table_pic"))) {
+      table_value = $(ui.draggable).closest('tr').find("td:first-child").text();
+      $newDiv = $("<div></div>").addClass("table_pic")
+      $newDiv.append($("<label></label>").text(table_value));
+          
+      // var $newPosX = event.pageX - $(this).offset().left;
+      // var $newPosY = event.pageY - $(this).offset().top;
+      // $newDiv.css("left", $newPosX);
+      // $newDiv.css("top", $newPosY);
+      $newDiv.appendTo($(this));
+      
+      $(ui.draggable).closest('tr').remove();
+
+    }
+    
+
+      
  
     }
 });
