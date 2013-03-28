f6ae1f873126be12a57e417aeabb6f6bc5277c9c
Avinash kaur <avinash.kaur@vinsol.com>
2013-03-28 16:02:57 +0530

    28/03-I

--- TableSolution/app/assets/javascripts/application.js
+++ TableSolution/app/assets/javascripts/application.js
@@ -18,4 +18,5 @@
 // $(function() {
 //     $( "#datepicker" ).datepicker();
 //   });
+$("#tabs ul").tabs({ active: 1 });
 
--- TableSolution/app/assets/javascripts/tables.js
+++ TableSolution/app/assets/javascripts/tables.js
@@ -8,39 +8,67 @@ $(document).ready( function() {
     });
   });
 
-  // $("#floor_image_div #slot_container").delegate("div", "drag", function(){
-  //   $(this).draggable();
-  // });
+  
+  // $length = $("#floor_image_div .slot").length;
+  // for ( i = 1; i <= $length; i++) {
+  //   $("#floor_image_div #slot" + i).droppable({
+  //     accept: '#unassigned tr td',
+  //     drop: handleTableDrop,
+  //     hoverClass: "hovered"
+  //   });
+  // }
 
-  $length = $("#floor_image_div .slot").length;
-  for ( i = 1; i <= $length; i++) {
-    $("#floor_image_div #slot" + i).droppable({
-      accept: '#unassigned tr td',
+  
+
+  $("#floor_image_div").delegate("div[class=table_pic]", "mousedown", function(){
+    $(this).draggable({
+      stack: '#floor_image_div .table_pic',
+      cursor: 'move',
+      revert: true,
+      // stop: function(event, ui) {
+      //   console.log($(this).closest("div"))
+      // }
+    });
+  });
+
+  
+
+  $("#floor_image_div .slot").each(function() {
+    $(this).droppable({
+      accept: "#unassigned tr td, #floor_image_div .table_pic",
       drop: handleTableDrop,
       hoverClass: "hovered"
     });
-  }
-  
+  });
 
   function handleTableDrop(event, ui) {
+        
     if (!($(ui.draggable).hasClass("table_pic"))) {
+      $(ui.draggable).closest('tr').detach().css({top: 0,left: 0});
       table_value = $(ui.draggable).closest('tr').find("td:first-child").text();
-      $newDiv = $("<div></div>").addClass("table_pic")
-      $newDiv.append($("<label></label>").text(table_value));
-          
-      // var $newPosX = event.pageX - $(this).offset().left;
-      // var $newPosY = event.pageY - $(this).offset().top;
+      
+    }
+    else {
+      table_value = $(ui.draggable).find('label').text();
+      $dragged_id = $(ui.draggable).closest('.slot').attr("id");
+      $dropped_id = $(this).attr("id");
+      if ($dragged_id != $dropped_id) {
+        $(ui.draggable).closest('.slot').droppable("enable");
+      }
+      $(ui.draggable).remove();
+    }
+
+    $newDiv = $("<div>").addClass("table_pic") 
+    $newDiv.append($("<label>").text(table_value));
+    $newDiv.appendTo($(this));
+    $(ui.draggable).draggable({"revert": false });
+    $(this).droppable("disable");
+      
+    var $newPosX = $(this).offset().left - $(this).closest('#floor_image_div').offset().left;
+    var $newPosY = $(this).offset().top - $(this).closest('#floor_image_div').offset().top;
       // $newDiv.css("left", $newPosX);
       // $newDiv.css("top", $newPosY);
-      $newDiv.appendTo($(this));
-      
-      $(ui.draggable).closest('tr').remove();
-
-    }
     
-
-      
- 
     }
 });
 
