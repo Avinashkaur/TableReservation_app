e86a67adec8f75030ccd1b9cda52759417280079
Avinash Kaur <avinash@avinash-HP-430-Notebook-PC.(none)>
2013-04-02 19:08:48 +0530

    search_customer

--- TableSolution/app/assets/javascripts/reservations.js
+++ TableSolution/app/assets/javascripts/reservations.js
@@ -1,26 +1,23 @@
 $(document).ready(function() {
-  
-  
 
+	$("#datepicker").datepicker();
+  
   $("#tags").autocomplete({
   	appendTo: "#search_box",
-  	source: getCustomerNames()  	
+  	source: getCustomerNames()
+  	
   });
-
-
-
-
-showNames();
+  showNames();
+  makeNamesDraggable();
 
 });
 
 var getCustomerNames = function() {
-	arr = [];
+	names = [];
 	$("#cust_list ul li").each(function() {
-      arr.push($(this).text());
+      names.push($(this).text());
 	});
-
-	return arr;
+	return names;
 }
 
 var clearList = function() {
@@ -28,13 +25,29 @@ var clearList = function() {
 }
 
 var showNames = function() {
-	$("#tags").change(function() {
-		// clearList();
-		names = getCustomerNames();
-		names.each(function() {
-			console.log($(this));
+	$("#search_box").delegate("#tags" , "keydown", function(e) {
+		key = e.charCode || e.keyCode || 0;
+		value = $(this).val();
+		len = value.trim().length;
 
+		if ((key == 13) && (len != 0)) {
+			$("#cust_list ul").find(":not(li[value=" + value + "])").hide();
+			$("#cust_list ul").find("li[value=" + value + "]").show();
+		}
+		else {
+			$("#cust_list ul li").each(function() {
+				$(this).show();
+			});
+		}
+  });
+}
+
+var makeNamesDraggable = function() {
+	$("#cust_list ul").delegate("li", "mousedown", function() {
+		$(this).draggable({
+			stack: "#cust_list ul li",
+			revert: true
 		});
-
 	});
+
 }
